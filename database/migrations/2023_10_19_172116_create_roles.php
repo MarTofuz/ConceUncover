4b90af09f428b0a25673c0e7aee0ec9c31a8503e
MartoVbos <martin.villalobos@virginiogomez.cl>
2023-10-20 21:55:16 -0300

    Se corrigio error migration

--- database/migrations/2023_10_19_172116_create_roles.php
+++ database/migrations/2023_10_19_172116_create_roles.php
@@ -1,29 +1,54 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        $role1 = Role::create(['name' => 'admin']);
+        // Intentar encontrar el usuario con ID 1
         $user = User::find(1);
-        $user->assignRole($role1);
+
+        if ($user) {
+            // El usuario se encontró, asignar el rol
+            $role1 = Role::where('name', 'admin')->first();
+            $user->assignRole($role1);
+        } else {
+            // El usuario no se encontró, ejecutar seeders
+            Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
+
+            // Volver a intentar encontrar el usuario
+            $user = User::find(1);
+
+            if ($user) {
+                // Si el usuario se encontró después de ejecutar los seeders, asignar el rol
+                $role1 = Role::where('name', 'admin')->first();
+                $user->assignRole($role1);
+            } else {
+                // Manejar el caso en el que el usuario sigue sin encontrarse
+                // Esto podría incluir registros de error o excepciones.
+            }
+        }
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
-        //
+        // Puedes revertir la asignación del rol en caso de un "down".
+        $user = User::find(1);
+        if ($user) {
+            $role1 = Role::where('name', 'admin')->first();
+            $user->removeRole($role1);
+        }
     }
 };
+
