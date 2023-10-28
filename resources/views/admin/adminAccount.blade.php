ed0876330148dbb3946e01d9ccbbf1141ea0a803
MartoVbos <martin.villalobos@virginiogomez.cl>
2023-10-27 21:41:48 -0300

    [style] cards para usuarios y tiendas (admin)

--- resources/views/admin/adminAccount.blade.php
+++ resources/views/admin/adminAccount.blade.php
@@ -4,7 +4,6 @@
 
 @section('content_header')
     <h1>Gestión de Cuentas</h1>
-    <h1>Cualquier cambio</h1>
 @stop
 
 @section('content')
@@ -22,34 +21,41 @@
         </div>
     @endif
 
+    <div class="row">
     @foreach ($users as $user)
-    <div class="card">
-        <div class="card-header">
-            <h5 class="card-title">{{ $user->name }}</h5>
+        <div class="col-md-4">
+            <div class="card custom-card" style="width: 18rem;">
+                <h5 class="card-title text-center">{{ $user->name }}</h5>
+                <img class="card-img-top" src="{{ asset('img/avatar.jpg') }}" alt="Card image cap" height="170">
+                <div class="card-body">
+                    @if ($user->phone)
+                        <p>Teléfono: {{ $user->phone }}</p>
+                    @else
+                        <p>Teléfono: no disponible</p>
+                    @endif
+
+                    @if ($user->address)
+                        <p>Dirección: {{ $user->address }}</p>
+                    @else
+                        <p>Dirección: no disponible</p>
+                    @endif
+                    <p>Correo: {{ $user->email }}</p>
+                </div>
+                <a href="{{ route('eliminar-usuario', ['id' => $user->id]) }}" class="btn btn-danger">Eliminar</a>
+            </div>
         </div>
-        <div class="card-body">
-            @if ($user->phone)
-                <p>Teléfono: {{ $user->phone }}</p>
-            @else
-                <p>Teléfono: no disponible</p>
-            @endif
-            
-            @if ($user->address)
-                <p>Dirección: {{ $user->address }}</p>
-            @else
-                <p>Dirección: no disponible</p>
-            @endif
-            <p>Correo: {{ $user->email }}</p>
-        </div>
-        <div class="modal-footer">
-            <a href="{{ route('eliminar-usuario', ['id' => $user->id]) }}" class="btn btn-danger">Eliminar</a>
-        </div>
-    </div>
     @endforeach
+</div>
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <style>
+    .custom-card {
+        width: 250px; /* Ancho personalizado */
+        height: 400px; /* Alto personalizado */
+    }
+</style>
 @stop
 
 @section('js')
--- resources/views/admin/adminStore.blade.php
+++ resources/views/admin/adminStore.blade.php
@@ -22,42 +22,50 @@
     @endif
 
     @if($tiendas->isNotEmpty())
+    <div class="row">
         @foreach($tiendas as $tienda)
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">{{ $tienda->name }}</h5>
-                </div>
-                <div class="card-body">
-                    <p>Dirección: {{ $tienda->address }}</p>
-                    <p>Descripción: {{ $tienda->description }}</p>
-                    <p>Creado por: {{ $tienda->propietario->name }}</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="{{ route('eliminar-tienda', ['id' => $tienda->id]) }}" class="btn btn-danger">Eliminar</a>
-                </div>
-                <div class="modal-footer"><!-- Para separar --></div>
-                <div class="card-body">
-                    @if ($tienda->sucursales->isNotEmpty())
-                        @foreach ($tienda->sucursales as $sucursal)
-                            <p>Sucursal: {{ $sucursal->name }}</p>
-                            <p>Dirección: {{ $sucursal->address }}</p>
-                            <p>Descripción: {{ $sucursal->description }}</p>
-                            @if ($tienda->sucursales->isNotEmpty())
-                            <a href="{{ route('eliminar-sucursal', ['id' => $sucursal->id]) }}" class="btn btn-danger">Eliminar</a>
-                            @endif
-                        @endforeach
-                    @else
-                        <p>No hay sucursales en esta tienda.</p>
-                    @endif
+            <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header">
+                        <h5 class="card-title">{{ $tienda->name }}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p>Dirección: {{ $tienda->address }}</p>
+                        <p>Descripción: {{ $tienda->description }}</p>
+                        <p>Creado por: {{ $tienda->propietario->name }}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <a href="{{ route('eliminar-tienda', ['id' => $tienda->id]) }}" class="btn btn-danger">Eliminar</a>
+                    </div>
+                    <div class="modal-footer"><!-- Para separar --></div>
                 </div>
             </div>
+            
+            @if ($tienda->sucursales->isNotEmpty())
+                @foreach ($tienda->sucursales as $sucursal)
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-header">
+                            <h5 class="card-title">Sucursal</h5>
+                            </div>
+                            <div class="card-body">
+                            <p>{{ $sucursal->name }}</p>
+                                <p>Dirección: {{ $sucursal->address }}</p>
+                                <p>Descripción: {{ $sucursal->description }}</p>
+                            </div>
+                            <div class="modal-footer">
+                                <a href="{{ route('eliminar-sucursal', ['id' => $sucursal->id]) }}" class="btn btn-danger">Eliminar</a>
+                            </div>
+                            <div class="modal-footer"><!-- Para separar --></div>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         @endforeach
+    </div>
     @endif
 @stop
+
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+</style>
